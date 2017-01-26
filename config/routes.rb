9f093d2a7931f81baa982ae33bb04ee4d78dd21d
suilulu <m13869841793@gmail.com>
2017-01-26 12:32:20 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
+
+    root 'welcome#index'
+
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+        resources :resumes
     end
   end
+
 
 namespace :admin do
   resources :jobs
 end
+resources :jobs do
+  resources :resumes
+end
 
-    resources :jobs
-  root 'jobs#index'
+
+
 end
