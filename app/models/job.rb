085197367b513741e21f63029a9ae7469b868319
suilulu <m13869841793@gmail.com>
2017-01-29 22:34:39 +0800

    html beautify1

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,11 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, numericality: { greater_than: 0}
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
+  has_many :resumes
+   def self.search(search)
+      where("title LIKE ?", "%#{search}%").or(where("description LIKE ?", "%#{search}%"))
+   end
+   
   def publish!
     self.is_hidden = false
     self.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
 
     root 'welcome#index'
 
-  namespace :admin do
+    namespace :admin do
+        resources :jobs do
+            member do
+                post :publish
+                post :hide
+            end
+            resources :resumes
+        end
+    end
+
+    namespace :admin do
+        resources :jobs
+    end
     resources :jobs do
-      member do
-        post :publish
-        post :hide
-      end
         resources :resumes
     end
-  end
-
-
-namespace :admin do
-  resources :jobs
-end
-resources :jobs do
-  resources :resumes
-end
-
-
-
 end
